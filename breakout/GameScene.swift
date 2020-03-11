11938188a46cf32f033da0499b87b28280ad40f3
Jack <jkafka@bsd220.org>
2020-03-11 10:34:56 -0500

    created ball object

--- breakout/GameScene.swift
+++ breakout/GameScene.swift
@@ -11,27 +11,48 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-       
+    var ball = SKShapeNode()
+    
     override func didMove(to view: SKView) {
         createBackground()
+        makeBall()
         
     }
     func createBackground() {
-       let stars = SKTexture(imageNamed: "stars")
-       for i in 0...1 {
-          let starsBackground = SKSpriteNode(texture: stars)
-          starsBackground.zPosition = -1
-          starsBackground.position = CGPoint(x: 0, y: starsBackground.size.height * CGFloat(i))
-          addChild(starsBackground)
-          let moveDown = SKAction.moveBy(x: 0, y: -starsBackground.size.height, duration: 20)
-          let moveReset = SKAction.moveBy(x: 0, y: starsBackground.size.height, duration: 0)
-          let moveLoop = SKAction.sequence([moveDown, moveReset])
-          let moveForever = SKAction.repeatForever(moveLoop)
-          starsBackground.run(moveForever)
-       }
-    }
-
+        let stars = SKTexture(imageNamed: "stars")
+        for i in 0...1 {
+            let starsBackground = SKSpriteNode(texture: stars)
+            starsBackground.zPosition = -1
+            starsBackground.position = CGPoint(x: 0, y: starsBackground.size.height * CGFloat(i))
+            addChild(starsBackground)
+            let moveDown = SKAction.moveBy(x: 0, y: -starsBackground.size.height, duration: 20)
+            let moveReset = SKAction.moveBy(x: 0, y: starsBackground.size.height, duration: 0)
+            let moveLoop = SKAction.sequence([moveDown, moveReset])
+            let moveForever = SKAction.repeatForever(moveLoop)
+            starsBackground.run(moveForever)
         }
+    }
     
+    func makeBall() {
+        ball = SKShapeNode(circleOfRadius: 10)
+        ball.position = CGPoint(x: frame.midX, y: frame.midY)
+        ball.strokeColor = .black
+        ball.fillColor = .yellow
+        ball.name = "ball"
+        
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: 10)
+        ball.physicsBody?.isDynamic = false
+        ball.physicsBody?.usesPreciseCollisionDetection = true
+        ball.physicsBody?.friction = 0
+        ball.physicsBody?.affectedByGravity = false
+        ball.physicsBody?.restitution = 1
+        ball.physicsBody?.linearDamping = 0
+        ball.physicsBody?.contactTestBitMask = (ball.physicsBody?.collisionBitMask)!
+        
+        addChild(ball)
+        
+    }
+}
+
 
 
